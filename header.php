af2ba1714fc42fa7b19829939572639608f3cb6f
Sam Seaborn <pcisnerosfernandez@g.harvard.edu>
2014-10-03 01:19:44 -0500

    changed header

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
-<link rel="stylesheet" type="text/css" href="css/styles.css">
+<link rel="stylesheet" type="text/css" href="styles.css">
 
 
 </head>
